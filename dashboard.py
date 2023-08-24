cc76c7d61a4029581f96e36fa1c9d11e0ef3c6f8
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2023-08-24 15:25:33 +0800

    Added eda notebook and basic charts

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,32 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 from matplotlib import pyplot as plt 
 
+st.set_page_config(layout='wide')
 st.title('💻PC Express Graphics Cards Dashboard')
+
+col1, col2 = st.columns(2)
 
 df = pd.read_csv('pcexpress.csv')
 
-df2 = df.groupby('brand').size()
-df2 = df2.sort_values(ascending=False)
+brandcount = df.groupby('brand').size()
+brandcount = brandcount.sort_values(ascending=False)
+figbrandcount = px.bar(brandcount)
 
-st.bar_chart(df2)
+brandavgprice = df.groupby('brand')['price'].mean(numeric_only=True)
+brandavgprice = brandavgprice.sort_values(ascending=True)
 
-df4 = df.groupby('brand').mean(numeric_only=True)
-df4 = df4.sort_values('price', ascending=False)
+figbrandavgprice = px.bar(brandavgprice, x='price')
+figbrandavgprice.update_traces(marker_color='#f5054f')
 
-st.bar_chart(df4)
+scatterbrandprice = px.scatter(df, x='brand', y='price', color='vram')
+
+
+
+with col1:
+  st.plotly_chart(figbrandcount, theme='streamlit')
+  st.plotly_chart(scatterbrandprice, theme='streamlit')
+
+with col2:
+  st.plotly_chart(figbrandavgprice, theme='streamlit')
