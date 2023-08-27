b65030c2ebd4e775447f0378dcc0068dc7434e08
Denzel <denzel_bryan_yongco@dlsu.edu.ph>
2023-08-27 15:18:36 +0800

    Updated dashboard and eda

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title='GPU prices dashboard', layout='wide')
+st.set_page_config(page_title='GPU prices dashboard', page_icon='💻', layout='wide')
 st.title('GPU Prices Dashboard')
 
 col1, col2, col3, col4 = st.columns(4)
@@ -30,33 +30,48 @@ brandavgprice_df = gpu_df.groupby('brand')['price'].mean(numeric_only=True)
 brandavgprice_df = brandavgprice_df.sort_values(ascending=True)
 
 modelavgprice_df = gpu_df.groupby('model')['price'].mean(numeric_only=True)
-modelavgprice_df = modelavgprice_df.sort_values(ascending=True)
+modelavgprice_df = modelavgprice_df.sort_values(ascending=False)
+
+
+modelcount_df = gpu_df.groupby('model').size()
+modelcount_df = modelcount_df.reset_index()
+
+modelcount_df = modelcount_df.rename(columns={0: 'count'})
+modelcount_df = modelcount_df.sort_values(by='count', ascending=False)
+
+figmodelcount = px.bar(modelcount_df, x='model', y='count', title='Number of items per model')
 
 figbrandavgprice = px.bar(brandavgprice_df, x='price', title='Average prices per brand')
 
 figmodelavgprice = px.bar(modelavgprice_df, title='Average prices per model')
 
 figpricedist = px.histogram(gpu_df, x='price', nbins=30, title='Price distribution of all items')
-figpricedist.update_traces(marker_line_width=1,marker_line_color="white")
+figpricedist.update_traces(marker_line_width=1, marker_line_color="white")
 
 
 
-figscatterbrandpricemodel = px.scatter(gpu_df, x='brand', y='price', color='model', hover_data=['name', 'memory size', 'memory type'], title='All items', height=750)
-figscatterbrandpricemodel.update_traces(marker_size=20)
+
 with cola:
   st.plotly_chart(figbrandcount, theme='streamlit')
-  NExpensive = st.slider('Number of items', 1, int(len(gpu_df)/2), 10, key='expensive')
+  NExpensive = st.slider('Select number of items', 1, int(len(gpu_df)/2), 10, key='expensive')
   figtopNExpensive = px.bar(gpu_df.nlargest(int(NExpensive), 'price').sort_values(ascending=True, by='price'), x='price', y='name', title=f'Top {NExpensive} most expensive items')
   figtopNExpensive.update_layout(minreducedheight=500, height=700)
   st.plotly_chart(figtopNExpensive, theme='streamlit')
-  st.plotly_chart(figpricedist, theme='streamlit')
+  st.plotly_chart(figmodelcount, theme='streamlit')
+
 with colb:
   st.plotly_chart(figbrandavgprice, theme='streamlit')
-  NCheapest = st.slider('Number of items', 1, int(len(gpu_df)/2), 10, key='cheapest')
+  NCheapest = st.slider('Select number of items', 1, int(len(gpu_df)/2), 10, key='cheapest')
   figtopNCheapest = px.bar(gpu_df.nsmallest(int(NCheapest), 'price').sort_values(ascending=False, by='price'), x='price', y='name', title=f'Top {NCheapest} most affordable items')
   figtopNCheapest.update_layout(minreducedheight=500, height=700)
   st.plotly_chart(figtopNCheapest, theme='streamlit')
   st.plotly_chart(figmodelavgprice, theme='streamlit')
+
+st.plotly_chart(figpricedist, theme='streamlit', use_container_width=True)
+modeloptions = st.multiselect('Select model', modelcount_df['model'], modelcount_df['model'])
+
+figscatterbrandpricemodel = px.scatter(gpu_df.loc[gpu_df['model'].isin(modeloptions)], x='brand', y='price', color='model', hover_data=['name', 'memory size', 'memory type'], title='All items', height=750)
+figscatterbrandpricemodel.update_traces(marker_size=20)
 
 st.plotly_chart(figscatterbrandpricemodel, theme='streamlit', use_container_width=True)
 st.write('You can check this project on [Github](https://github.com/dyitsme/gpu_specs_prices_dashboard).')
